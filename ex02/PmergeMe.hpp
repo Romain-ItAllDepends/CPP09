74226dd26b084ee2275be3819f95f8591bdcb6c6
Romain <romain.gobet30@hotmail.com>
2025-01-02 13:01:06 +0100

    doing sequences

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -51,11 +51,8 @@ class PmergeMe
 			M mini;
 
 			for (std::size_t i = 0; i < maxi.size() - 1; i += 2)
-			{
 				pair.push_back(std::make_pair(std::max(maxi[i], maxi[i + 1]), std::min(maxi[i], maxi[i + 1])));
-				mini.push_back(pair.back().second);
-			}
-			if (maxi.size() == 3 || (mini.empty() == false && *std::max_element(mini.begin(), mini.end()) < maxi.back() && maxi.size() % 2 == 1))
+			if (mini.empty() == false && *std::max_element(mini.begin(), mini.end()) < maxi.back() && maxi.size() % 2 == 1)
 				pair.push_back(std::make_pair(std::max(pair.back().second, maxi.back()), -1));
 			else if (maxi.size() % 2 == 1) // Impair == min if size > 3 / test if sort 3 values
 				pair.push_back(std::make_pair(-1, maxi.back())); // Pair with smaller min
@@ -64,7 +61,10 @@ class PmergeMe
 			{
 				if (pair.size() - size % 2 == 1 && i == pair.size()) // exclude the last element because it's the place of impair element
 					continue ;
-				maxi.push_back(pair[i].first);
+				if (pair[i].first != -1)
+					maxi.push_back(pair[i].first);
+				if (pair[i].second != -1)
+					mini.push_back(pair[i].second);
 			}
 
 			// make re && ./PmergeMe 3 10 8 18 4 16 12 13 2 15 7 9 20 17 1 19 11 14 6 5 88
@@ -86,33 +86,82 @@ class PmergeMe
 		}
 
 		// Norme
-		size_t test(size_t n)
+		template< typename M >
+		static M suitJacobsthal(M &mini)
 		{
-			std::vector<int> jacob(2, 2);
-			size_t tmp = 2;
+			M jacob(2, 2);
+			std::size_t tmp = 2;
+			std::size_t const n = mini.size();
 
-			for (size_t i = 2 ; i < n && tmp <= n ; i++)
+			for (std::size_t i = 2 ; i < n && tmp <= n ; i++)
 			{
 				tmp = jacob[i - 1] + 2 * jacob[i - 2];
 				jacob.push_back(tmp);
-				std::cout << BLUE << n << " TEST N: " << tmp << NC << std::endl;
 			}
-			return tmp;
+			return jacob;
 		}
+
+		template< typename M >
+		static void setupSections(M &split, M &mini)
+			{
+				std::size_t count = 0;
+				int j;
+				M tmp;
+
+				for (std::size_t i = 0; i < mini.size() && i < 4; i += 2)
+				{
+					if (mini.size() > i + 1)
+						tmp.push_back(mini[i + 1]);
+					tmp.push_back(mini[i]);
+					count += 2;
+				}
+				for (std::size_t n = 2 ; n < split.size() && mini.size() > 4 ; n++)
+				{
+					if (mini.size() > count + split[n])
+						j = count + split[n];
+					else
+						j = mini.size() - 1;
+					for (std::size_t i = j ; i <= count ; --i)
+					{
+						tmp.push_back(mini[i]); // Error here
+					}
+					count += split[n];
+				}
+				mini = tmp;
+			}
 
 		template< typename M >
 		M sortAndMerge(M &maxi, M &mini)
 		{
+			M split;
+
 			// print min
+			std::cout << std::endl << RED << mini.size()  << " START" << std::endl;
+			std::cout << std::endl;
 			for (std::size_t i = 0; i < mini.size(); i++)
-				std::cout << BLUE << mini[i] << NC << std::endl;
+				std::cout << BLUE << mini[i] << ", " << NC;
+			std::cout << std::endl;
 			// end print
+
+			if (mini.empty() == true)
+				throw std::string("Error: An empty container has been detected!");
 			if (maxi.size() == 1)
-			{
 				maxi.push_back(mini.back());
-				return maxi;
-			}
-			test(mini.size());
+			else
+				split = suitJacobsthal(mini);
+			if (maxi.size() != 1)
+				setupSections(split, mini);
+
+			// print min
+			std::cout << std::endl << RED << mini.size() << " END" << std::endl;
+			std::cout << std::endl;
+			for (std::size_t i = 0; i < mini.size(); i++)
+				std::cout << BLUE << mini[i] << ", " << NC;
+			std::cout << std::endl;
+			// end print
+
+			// Need to merge
+
 			// Coupe min en groupe (avec la suite de jacobsthal) et les inverse (les nombres de chaque groupe)
 			// insert chaque nombre en utilisant binary search
 			return maxi;
